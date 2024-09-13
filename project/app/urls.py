c7ca1c281cf0b02529c901cd5944906e06207ee3
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-13 22:23:50 +0530

    commit

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,6 +3,6 @@ from app import views
 
 urlpatterns=[
     path('', views.home, name='home'),
-    # path('register/', views.register, name='register'),
-    # path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
