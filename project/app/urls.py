d4672737a62249788e705a19c8b67556e646f67d
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-24 15:43:48 +0530

    Registration And Login By DTL

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,5 +5,6 @@ urlpatterns=[
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    path('registerdata/', views.registerdata, name="registerdata")
+    path('registerdata/', views.registerdata, name="registerdata"),
+    path('logout/', views.logout, name='logout'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -3,37 +3,46 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
-def register(request):
-    return render(request,'register.html')
+
+
+        
+       
+        
+   
+
+    
 
 
 def login(request):
-    if request.method=='POST':
-        email=request.POST.get('email')
-        password=request.POST.get('password')
-        # print(email,password)
-        name1=request.COOKIES['name']
-        email1=request.COOKIES['email']
-        contact1=request.COOKIES['contact']
-        password1=request.COOKIES['password']
-        # print(name,email,contact,password)
-        if email1==email:
-            if password1==password:
-                data={
-                    'nm':name1,
-                    'em':email1,
-                    'con':contact1,
-                    'pas':password1
-                }
-                return render(request,'deshbord.html',data)
-            else:
-                msg="email and password not match"
-                return render(request,'login.html',{'msg':msg}) 
-        else:
-            msg='email id not registerd'
-        return render(request,'login.html',{'msg':msg})
-    else:
-        return render(request,'login.html')
+    # if request.method=='POST':
+    #     email=request.POST.get('email')
+    #     password=request.POST.get('password')
+    #     # print(email,password)
+    #     name1=request.COOKIES['name']
+    #     email1=request.COOKIES['email']
+    #     contact1=request.COOKIES['contact']
+    #     password1=request.COOKIES['password']
+    #     # print(name,email,contact,password)
+    #     if email1==email:
+    #         if password1==password:
+    #             data={
+    #                 'nm':name1,
+    #                 'em':email1,
+    #                 'con':contact1,
+    #                 'pas':password1
+    #             }
+    #             return render(request,'deshbord.html',data)
+    #         else:
+    #             msg="Password not match"
+    #             return render(request,'login.html',{'msg':msg}) 
+    #     else:
+    #         msg='email id not registerd'
+    #     return render(request,'login.html',{'msg':msg})
+    # else:
+    #     return render(request,'login.html')
+        
+        
+    
 
 def registerdata(request):
     print(request.method)
@@ -51,11 +60,18 @@ def registerdata(request):
     print(contact)
     print(password)
 
-    response=render(request,'login.html')
+    response=render(request,'login.html')  
     response.set_cookie('name',name)
     response.set_cookie('email',email)
     response.set_cookie('contact',contact)
     response.set_cookie("password",password)
+    return response
+def logout(request):
+    response=render(request,'home.html')
+    response.delete_cookie('name')
+    response.delete_cookie('contact')
+    response.delete_cookie('email')
+    response.delete_cookie('password')
     return response
 
  
