7eadc9668cd685d7e4175aa39742631fcc5990af
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-25 08:31:36 +0530

    Registration And Login By DTL

--- project/app/views.py
+++ project/app/views.py
@@ -5,12 +5,28 @@ def home(request):
     return render(request,'home.html')
 
 def register(request):
+    # if request.method=='POST':
+    #     name=request.POST.get('name')
+    #     email=request.POST.get('email')
+    #     contact=request.POST.get('contact')
+    #     password=request.POST.get('password')
+    #     print(name,email,contact,password)
+    #     data={
+    #         'name':name,
+    #         'email':email,
+    #         'contact':contact,
+    #         'password':password
+    #     }
+    #     request.session['data']=data
+    #     return render(request,'login.html')
+    # else:
+    #     return render(request,'register.html')
+
     if request.method=='POST':
         name=request.POST.get('name')
         email=request.POST.get('email')
         contact=request.POST.get('contact')
         password=request.POST.get('password')
-        print(name,email,contact,password)
         data={
             'name':name,
             'email':email,
@@ -56,6 +72,21 @@ def login(request):
         data1=request.session.get('data')
         print(data1)
         print(data1["name"],data1["email"],data1["contact"],data1["password"])
+        if data1['email']==email:
+            if data1['password']==password:
+                my_data={
+                    'nm':data1['name'],
+                    'em':data1['email'],
+                    'con':data1['contact'],
+                    'pas':data1['password']
+                }
+                return render(request,'deshbord.html',my_data)
+            else:
+                msg="Password is inccorrect"
+                return render(request,'login.html',{'msg':msg})
+        else:
+            msg="Email is incorrect"
+            return render(request,'login.html',{'msg':msg})
     else:
         return render(request,'login.html')
 
