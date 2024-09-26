148241ab5cfc974a8198df44c50bade84a62bdb6
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-26 11:08:29 +0530

    Registration And Login By DTL(Session Storage)

--- project/app/views.py
+++ project/app/views.py
@@ -39,33 +39,6 @@ def register(request):
         return render(request,'register.html')
 
 def login(request):
-    # if request.method=='POST':
-    #     email=request.POST.get('email')
-    #     password=request.POST.get('password')
-    #     # print(email,password)
-    #     name1=request.COOKIES['name']
-    #     email1=request.COOKIES['email']
-    #     contact1=request.COOKIES['contact']
-    #     password1=request.COOKIES['password']
-    #     # print(name,email,contact,password)
-    #     if email1==email:
-    #         if password1==password:
-    #             data={
-    #                 'nm':name1,
-    #                 'em':email1,
-    #                 'con':contact1,
-    #                 'pas':password1
-    #             }
-    #             return render(request,'deshbord.html',data)
-    #         else:
-    #             msg="Password not match"
-    #             return render(request,'login.html',{'msg':msg}) 
-    #     else:
-    #         msg='email id not registerd'
-    #     return render(request,'login.html',{'msg':msg})
-    # else:
-    #     return render(request,'login.html')
-
     if request.method=='POST':
         
         data1=request.session.get('data')
