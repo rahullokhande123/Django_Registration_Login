4d53dab319eb8d0611ef7452b80d54481e63a2b6
Rahul Lokhande <rlokhande06244@gmail.com>
2024-09-26 11:10:37 +0530

    Registration And Login By DTL(Session Storage)

--- project/app/urls.py
+++ project/app/urls.py
@@ -5,6 +5,5 @@ urlpatterns=[
     path('', views.home, name='home'),
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
-    # path('registerdata/', views.registerdata, name="registerdata"),
     path('logout/', views.logout, name='logout'),
 ]
